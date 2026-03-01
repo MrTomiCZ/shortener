cce90106c43adea43c5bde87a6bbbc7fdad021d1
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2026-03-01 15:23:00 +0100

    Add URL shortener handler in short.js
    Implement URL shortening functionality using short.io

---
+++ api/short.js
@@ -0,0 +1,21 @@
+const shortio = require("short.io")
+const short = new shortio("mtmi.eu", 1303009, process.env.SHORTIO);
+
+function generateId(length = 6) {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let result = ""
+
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+
+    return result
+}
+
+export default function handler(req, res) {
+    const link = await short.createLink({
+        originalURL: req.query.url,
+        title: "Shortened By bam",
+        path: "P-"+req.query.short || "P-"+generateId()
+    )};
+}
